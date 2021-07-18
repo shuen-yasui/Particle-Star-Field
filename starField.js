e88e5bb4ec1aad79b3b7ddf5f9cdf5f3368080e5
Shuen Yasui <shuenyasui@gmail.com>
2021-07-17 22:05:42 -0700

    Move particle logic to Particle class. Add movement and size change to particle

--- starField.js
+++ starField.js
@@ -1,25 +1,40 @@
 class Particle {
 	constructor(){
+    this.x=window.innerWidth/2;
+    this.y=window.innerHeight/2;
+    this.size=1;
 	}
-	update(){
+	update(c){
+    c.beginPath();
+    c.arc(this.x,this.y,this.size,0,Math.PI * 2,false);
+    c.strokeStyle = "white";
+    c.stroke();
+    c.fillStyle = "black";
+    c.fill();
+    c.closePath();
+    this.x+=5;
+    if (this.x>=window.innerWidth){
+      this.x=0;
+    }
+    this.size=(5+(this.x/window.innerWidth)*20);
 	}
 }
 function init(){
+  console.log("START");
+  particles.push(new Particle())
 	update();
 }
 function update(){
 	c.clearRect(0,0,window.innerWidth,window.innerHeight);
-  c.beginPath();
-  c.arc(window.innerWidth/2,window.innerHeight/2,10,0,Math.PI * 2,false);
-  c.strokeStyle = "white";
-  c.stroke();
-  c.fillStyle = "black";
-  c.fill();
-  c.closePath();
 	requestAnimationFrame(update);
+  for (var i=0;i<particles.length;i++){
+    P=particles[i]
+    P.update(c)
+  }
 }
 var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
+var particles=[]
 init();
