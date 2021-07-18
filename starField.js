c7d3b9cd50e119865ed4473aad9c4ef81c74022e
Shuen Yasui <shuenyasui@gmail.com>
2021-07-18 14:12:04 -0700

    Changed spawn to circle, moved to center of field. Changed Particle spawn point and target point to be based on circular field

--- starField.js
+++ starField.js
@@ -6,35 +6,38 @@ class Particle {
     this.target_y=0;
     this.angle=0;
     this.size=0.5;
-    this.speed=5;
+    this.vel=5;
+    this.accel=0;
     this.spawn();
   }
   spawn(){
-    this.x=spawnBox_x+(spawnBoxWidth*Math.random());
-    this.y=spawnBox_y+(spawnBoxHeight*Math.random());
-    this.target_x=((this.x-spawnBox_x)*scaleMultiplier_x)
-    this.target_y=((this.y-spawnBox_y)*scaleMultiplier_y)+(window.innerHeight/2)-((spawnBoxHeight*(scaleMultiplier_y/2)));
+    var randomRadians=Math.random()*(Math.PI*2);
+    var randomHypot=Math.random()*spawnRadius;
+    this.x=spawn_x+(randomHypot*Math.cos(randomRadians));
+    this.y=spawn_y+(randomHypot*Math.sin(randomRadians));
+    this.target_x=spawn_x+(10*randomHypot*Math.cos(randomRadians));
+    this.target_y=spawn_y+(10*randomHypot*Math.sin(randomRadians));
     this.angle=Math.atan2((this.target_y-this.y),(this.target_x-this.x));
   }
 	update(c){
     // Draw
     c.beginPath();
-    c.arc(this.x,this.y,this.size,0,Math.PI * 2,false);
+    c.arc(this.x,this.y,this.size,0,Math.PI*2,false);
     c.strokeStyle = "white";
     c.stroke();
     c.fillStyle = "black";
     c.fill();
     c.closePath();
     // Movement
-    this.x+=(this.speed*Math.cos(this.angle));
-    this.y+=(this.speed*Math.sin(this.angle));
+    this.x+=(this.vel*Math.cos(this.angle));
+    this.y+=(this.vel*Math.sin(this.angle));
     //console.log("X:"+this.x+" Y:"+this.y+" Target: "+this.target_x+","+this.target_y);
     // Check respawn
     if (this.x>=window.innerWidth || this.y<0 || this.y>=window.innerHeight){
       this.spawn();
     }
     // Update Size
-    this.size=(5+(this.x/window.innerWidth)*5);
+    //this.size=(1+(this.x/window.innerWidth)*1);
 	}
 }
 function init(){
@@ -50,7 +53,7 @@ function update(){
 	c.clearRect(0,0,window.innerWidth,window.innerHeight);
   // Draw starting rect
   c.beginPath();
-  c.rect(spawnBox_x,spawnBox_y,spawnBoxWidth,spawnBoxHeight);
+  c.arc(spawn_x,spawn_y,spawnRadius,0,Math.PI * 2,false);
   c.strokeStyle = "white";
   c.stroke();
   c.closePath();
@@ -65,13 +68,10 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
-var maxParticle=50;
+var maxParticle=100;
 var particles=[];
-// Spawn Box
-var scaleMultiplier_x=3;
-var scaleMultiplier_y=6;
-var spawnBoxWidth=window.innerWidth/scaleMultiplier_x;
-var spawnBoxHeight=window.innerHeight/scaleMultiplier_y;
-var spawnBox_x=0;
-var spawnBox_y=(window.innerHeight/2)-(spawnBoxHeight/2);
+// Spawn Circle
+var spawn_x=window.innerWidth/2;
+var spawn_y=window.innerHeight/2;
+var spawnRadius=100;
 init();
