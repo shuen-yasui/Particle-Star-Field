645464b88476fd6f8def70c88df5fe0788c69136
Shuen Yasui <shuenyasui@gmail.com>
2021-07-18 17:18:13 -0700

    Removed stroke from Particles, added fill.

--- starField.js
+++ starField.js
@@ -5,7 +5,7 @@ class Particle {
     this.target_x=0;
     this.target_y=0;
     this.angle=0;
-    this.size=0.5;
+    this.size=1;
     this.vel=0;
     this.default_vel=0.5;
     this.accel=0;
@@ -32,22 +32,17 @@ class Particle {
     // Draw
     c.beginPath();
     c.arc(this.x,this.y,this.size,0,Math.PI*2,false);
-    c.strokeStyle = "rgba(255, 255, 255, "+this.alpha+")";
-    c.stroke();
-    c.fillStyle = "black";
+    c.fillStyle = "rgba(255, 255, 255, "+this.alpha+")";
     c.fill();
     c.closePath();
     // Movement
     this.vel+=this.accel;
     this.x+=(this.vel*Math.cos(this.angle));
     this.y+=(this.vel*Math.sin(this.angle));
-    //console.log("X:"+this.x+" Y:"+this.y+" Target: "+this.target_x+","+this.target_y);
     // Check respawn
     if (this.x>=window.innerWidth || this.y<0 || this.y>=window.innerHeight){
       this.spawn();
     }
-    // Update Size
-    //this.size=(1+(this.x/window.innerWidth)*1);
 	}
 }
 function init(){
@@ -78,7 +73,7 @@ var canvas = document.querySelector('canvas');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 var c = canvas.getContext('2d');
-var maxParticle=800;
+var maxParticle=1000;
 var particles=[];
 // Spawn Circle
 var spawn_x=window.innerWidth/2;
